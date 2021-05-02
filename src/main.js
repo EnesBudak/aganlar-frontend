368282106322c3832864315269e85dc5b4129897
Enes Budak <enesbudak.mdbf17@iste.edu.tr>
2021-05-03 01:17:17 +0300

    test

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ import store from './store/index.js'
 import axios from 'axios'
 import ElementPlus from 'element-plus';
 import 'element-plus/lib/theme-chalk/index.css';
-// axios.defaults.baseURL = 'https://aganlar-backend.herokuapp.com/api/v1/'
-axios.defaults.baseURL = 'http://localhost:3001/api/v1/'
+axios.defaults.baseURL = 'https://aganlar-backend.herokuapp.com/api/v1/'
+// axios.defaults.baseURL = 'http://localhost:3001/api/v1/'
 
 
 createApp(App).use(ElementPlus).use(router).use(store).mount('#app')
