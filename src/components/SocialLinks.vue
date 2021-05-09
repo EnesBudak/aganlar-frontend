d07fda1ab4aa38f557b70dd6eb37a6f84be6396c
Enes Budak <enesbudak.mdbf17@iste.edu.tr>
2021-05-09 19:00:29 +0300

    update

--- src/components/SocialLinks.vue
+++ src/components/SocialLinks.vue
@@ -1,57 +1,5 @@
 <template>
   <Newsletter />
-  <div class="instagram_posts">
-    <div class="posts">
-      <a class="post">
-        <div class="sq_parent">
-          <div class="sq_wrap">
-            <div class="sq_content">
-              <img
-                src="http://yatvilla.com/wp-content/uploads/2021/03/home-face-bg-new.png?id=129"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </a>
-      <a class="post">
-        <div class="sq_parent">
-          <div class="sq_wrap">
-            <div class="sq_content">
-              <img
-                src="http://yatvilla.com/wp-content/uploads/2021/03/home-pint-bg-new.png?id=132"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </a>
-      <a class="post">
-        <div class="sq_parent">
-          <div class="sq_wrap">
-            <div class="sq_content">
-              <img
-                src="http://yatvilla.com/wp-content/uploads/2021/03/home-insta-bg-new.png?id=131"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </a>
-      <a class="post">
-        <div class="sq_parent">
-          <div class="sq_wrap">
-            <div class="sq_content">
-              <img
-                src="http://yatvilla.com/wp-content/uploads/2021/03/home-youtube-bg-new.png?id=130"
-                alt=""
-              />
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
-  </div>
 </template>
 
 <script>
