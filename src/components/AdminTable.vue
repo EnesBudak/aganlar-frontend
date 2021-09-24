e51d8d731a6bc73ba8b78d2c0e6887599018b043
Enes Budak <enesbudak.mdbf17@iste.edu.tr>
2021-09-24 15:17:21 +0300

    update

--- src/components/AdminTable.vue
+++ src/components/AdminTable.vue
@@ -57,6 +57,9 @@
    <el-form-item label="Ücret">
     <el-input v-model="yacht.price"></el-input>
   </el-form-item>
+   <el-form-item label="Döviz">
+    <el-input v-model="yacht.currency"></el-input>
+  </el-form-item>
    <el-form-item label="Dizayn">
     <el-input v-model="yacht.design"></el-input>
   </el-form-item>
@@ -195,6 +198,7 @@ import axios from 'axios'
           owner: '',
           categoryType: '',
           price: '',
+          currency:'',
           description:'',
           design: '',
           interiorDesign: '',
